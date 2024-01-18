078363bfefa90e52fda723db3ea78554d8172864
Kapilan199 <kapilanr2003@gmail.com>
2024-01-17 16:27:13 -0800

    edit app.py

--- app.py
+++ app.py
@@ -7,3 +7,16 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/login")
+def home():
+    return render_template("login.html")
+
+@app.route("/signup")
+def home():
+    return render_template("signup.html")
+
+
+@app.route("/")
+def home():
+    return render_template("splash.html")
