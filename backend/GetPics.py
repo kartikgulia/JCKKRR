2589e2c52f8c80072d54d393e9e63abe2264a04f
ca8maristela <57410056+ca8maristela@users.noreply.github.com>
2024-02-05 12:21:00 -0800

    Added function that outputs a document with a list of image urls

--- backend/GetPics.py
+++ backend/GetPics.py
@@ -8,24 +8,48 @@ import re
 # https://api.flickr.com/services
 
 
-AccessKey = '2df9cf762ba619075f2827fea8edff89'
+def GetJSON():
+    AccessKey = '2df9cf762ba619075f2827fea8edff89'
 
-GETrequest = requests.get(
-    f"https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={AccessKey}&format=json&tags=Historical")
+    GETrequest = requests.get(
+        f"https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={AccessKey}&format=json&tags=Historical")
 
-filterJSON = re.search(r'jsonFlickrApi\((.*)\)', GETrequest.text)
+    filterJSON = re.search(r'jsonFlickrApi\((.*)\)', GETrequest.text)
 
-# solution to formatting issue with json file returned by flickr
-if filterJSON:
-    jsonText = filterJSON.group(1)
-    filepaths = json.loads(jsonText)
+    # solution to formatting issue with json file returned by flickr
+    if filterJSON:
+        jsonText = filterJSON.group(1)
+        filepaths = json.loads(jsonText)
 
-# print(filepaths)
-# print(GETrequest.text)
-# FilePaths = GETrequest.json()
+    # print(filepaths)
+    # print(GETrequest.text)
 
-# for some reason API is not returning the urls to the images. Need to look into this
-file = 'ImageData'
+    JSONfile = 'ImageData'
 
-with open(file, 'w') as json_file:
-    json.dump(filepaths, json_file)
+    with open(JSONfile, 'w') as json_file:
+        json.dump(filepaths, json_file)
+
+    return JSONfile
+
+
+def GetURLS(JsonFile):
+    # url: https://live.staticflickr.com/{server_id}/{id}_{secret}.jpg
+
+    with open(JsonFile, 'r') as file:
+        data = json.load(file)
+
+    photos = data["photos"]["photo"]
+
+    with open('URLS', 'w') as file:
+        for dict in photos:
+            server_id = dict["server"]
+            id = dict["id"]
+            secret = dict["secret"]
+            url = f"https://live.staticflickr.com/{server_id}/{id}_{secret}.jpg"
+            url += '\n'
+            file.write(url)
+
+
+file = GetJSON()
+# print(file)
+GetURLS(file)
