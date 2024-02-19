a84fa9951bc2742ae512ef2c060003e64520e1cf
RayyanZaid <rayyanzaid0401@gmail.com>
2024-02-19 15:24:13 -0800

    created basic implementation for players

--- backend/UserModule/Player.py
+++ backend/UserModule/Player.py
@@ -9,11 +9,23 @@ cred = credentials.Certificate('jokerker-d9272-firebase-adminsdk-sbyd5-fda51193b
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+players_ref = db.collection('players')
+
+def get_players_data():
+    docs = players_ref.get()
+    documents_dict = {}
+    for idx, doc in enumerate(docs, start=1):
+        documents_dict[idx] = doc.to_dict()
+    return documents_dict
 
 class Player(UserInterface):
     def __init__(self, userID: str):
     
         self.userID = userID
+        self.gameIDsPlayed : str = []
+        self.name : str = ""
+        self.email : str = ""
+        self.score : int = 0
 
     def signIn(self, email: str, password: str):
         # Implement sign-in logic here
@@ -23,6 +35,10 @@ class Player(UserInterface):
 
 
         if signInSuccessful:
+
+            # populate the name, email, score, gameIDsPlayed fields
+
+            # Then, add the player object to the PlayerSessionManager array
             pass
 
 
