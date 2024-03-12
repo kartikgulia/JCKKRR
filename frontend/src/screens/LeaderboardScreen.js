68b42e4041b414acd7a1a789a7ff6372fae991a1
RayyanZaid <rayyanzaid0401@gmail.com>
2024-03-12 01:10:19 -0700

    allowed for multi pc access

--- frontend/src/screens/LeaderboardScreen.js
+++ frontend/src/screens/LeaderboardScreen.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import "../styles/LeaderboardScreen.css"; 
-import { Link } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import "../styles/LeaderboardScreen.css";
+import { Link } from "react-router-dom";
+import SERVER_URL from "../config";
 const LeaderboardTable = ({ data, difficulty }) => (
   <div className="leaderboard-container">
     <h2 className="leaderboard-difficulty">{difficulty}</h2>
@@ -38,50 +38,55 @@ const LeaderboardScreen = () => {
     const fetchLeaderboardData = async () => {
       try {
         // Fetch data from your Firestore database for Easy difficulty
-        const easyResponse = await fetch('http://127.0.0.1:5000/easyleaderboard');
+        const easyResponse = await fetch(`${SERVER_URL}/easyLeaderboard`);
         const easyData = await easyResponse.json();
-        
+
         // Convert to array and sort by score
         const easyArray = Object.values(easyData);
-        const sortedEasyData = easyArray.filter(entry => entry.Score).sort((a, b) => b.Score - a.Score);
+        const sortedEasyData = easyArray
+          .filter((entry) => entry.Score)
+          .sort((a, b) => b.Score - a.Score);
 
         const processedEasyData = sortedEasyData.map((entry, index) => ({
           rank: index + 1,
-          playerName: entry.Name, 
-          score: entry.Score, 
+          playerName: entry.Name,
+          score: entry.Score,
         }));
 
-
         // Fetch medium data
-        const mediumResponse = await fetch('http://127.0.0.1:5000/mediumleaderboard');
+        const mediumResponse = await fetch(`${SERVER_URL}/mediumleaderboard`);
         const mediumData = await mediumResponse.json();
 
         // Convert to array and sort by score
         const mediumArray = Object.values(mediumData);
-        const sortedDataMedium = mediumArray.filter(entry => entry.Score).sort((a, b) => b.Score - a.Score);
+        const sortedDataMedium = mediumArray
+          .filter((entry) => entry.Score)
+          .sort((a, b) => b.Score - a.Score);
 
         // Map array values
         const processedMediumData = sortedDataMedium.map((entry, index) => ({
           rank: index + 1,
-          playerName: entry.Name, 
-          score: entry.Score, 
+          playerName: entry.Name,
+          score: entry.Score,
         }));
 
         // Fetch hard data
-        const hardResponse = await fetch('http://127.0.0.1:5000/hardleaderboard');
+        const hardResponse = await fetch(`${SERVER_URL}/hardLeaderboard`);
         const hardData = await hardResponse.json();
 
         // Convert to array and sort by score
         const hardArray = Object.values(hardData);
-        const sortedDataHard = hardArray.filter(entry => entry.Score).sort((a, b) => b.Score - a.Score);
+        const sortedDataHard = hardArray
+          .filter((entry) => entry.Score)
+          .sort((a, b) => b.Score - a.Score);
 
         // Map array values
         const processedHardData = sortedDataHard.map((entry, index) => ({
           rank: index + 1,
-          playerName: entry.Name, 
-          score: entry.Score, 
+          playerName: entry.Name,
+          score: entry.Score,
         }));
-  
+
         setLeaderboardData({
           easy: processedEasyData,
           medium: processedMediumData,
@@ -90,7 +95,7 @@ const LeaderboardScreen = () => {
 
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching leaderboard data:', error);
+        console.error("Error fetching leaderboard data:", error);
         setLoading(false);
       }
     };
@@ -100,7 +105,9 @@ const LeaderboardScreen = () => {
 
   return (
     <div className="leaderboard-screen">
-      <Link to="/gameSelect" className="back-button">Back</Link>
+      <Link to="/gameSelect" className="back-button">
+        Back
+      </Link>
       <h1 className="leaderboard-title">Leaderboard</h1>
       {loading ? (
         <p>Loading leaderboard...</p>
