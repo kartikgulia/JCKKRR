dbf30aa3a4ccc24aeef974a508c8a05f08e27a18
ryannnlee1 <ryanduckylee@gmail.com>
2024-03-12 16:12:41 -0700

    "Multi Roound Testing Suite for Easy Game Scoring Comeplete"

--- backend/test_B.py
+++ backend/test_B.py
@@ -32,35 +32,37 @@ class TestEasyGame:
     def test_score_easyrounds_worounding(self):
         mock_player = Player("mRafsYCe9zWbCFNIcIIZhJoHSFn2")
         easy_game = EasyGame(mock_player)
-        easy_game.yearGuesses = [2010, 2010] # second in list should account for easy rounding
-        easy_game.targetGuesses = [[5,5]] # second in list should account for easy rounding
+        easy_game.yearGuesses = [2010, 2000] # second in list should account for easy rounding
+        easy_game.targetGuesses = [[5,5], [10,10]] # second in list should account for easy rounding
         # Mocking Round data
         class MockRound1:
             def getData(self):
                 return {"yearTaken": 2010, "targetImageCoordinates": [[0, 0], [0, 10], [10, 10], [10, 0]]}
         class MockRound2:
             def getData(self):
-                return {"yearTaken": 2010, "targetImageCoordinates": [[0, 0], [0, 10], [10, 10], [10, 0]]}
+                return {"yearTaken": 2000, "targetImageCoordinates": [[0, 0], [0, 20], [20, 20], [20, 0]]}
         easy_game.rounds = [MockRound1(), MockRound2()]
         total_score = easy_game.scoreRounds()
-        assert total_score[0] == 4048  # Total score for both rounds
+        assert total_score[0] == 8096  # Total score for both rounds
+        assert total_score[1] == [4048,4048]  # Score for each round
 
     # Test scoreRounds method for easy game WITH rounding (1 round)
     def test_score_easyrounds_wrounding(self):
         mock_player = Player("mRafsYCe9zWbCFNIcIIZhJoHSFn2")
         easy_game = EasyGame(mock_player)
-        easy_game.yearGuesses = [1961] # second in list should account for easy rounding
-        easy_game.targetGuesses = [[40,40]] # second in list should account for easy rounding
+        easy_game.yearGuesses = [1961, 2040] # second in list should account for easy rounding
+        easy_game.targetGuesses = [[40,40], [45,45]] # second in list should account for easy rounding
         # Mocking Round data
         class MockRound1:
             def getData(self):
                 return {"yearTaken": 2010, "targetImageCoordinates": [[0, 0], [0, 10], [10, 10], [10, 0]]}
         class MockRound2:
             def getData(self):
-                return {"yearTaken": 2010, "targetImageCoordinates": [[0, 0], [0, 10], [10, 10], [10, 0]]}
-        easy_game.rounds = [MockRound1()]
+                return {"yearTaken": 2000, "targetImageCoordinates": [[0, 0], [0, 20], [20, 20], [20, 0]]}
+        easy_game.rounds = [MockRound1(), MockRound2()]
         total_score = easy_game.scoreRounds()
-        assert total_score[0] == 4048  # Total score for both rounds
+        assert total_score[0] == 8096  # Total score for both rounds
+        assert total_score[1] == [4048,4048]  # Score for each round
 
     # Add more test cases for other scenarios and edge cases as needed
 
